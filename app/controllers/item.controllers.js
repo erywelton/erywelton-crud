5817e91cdacc558dd2f2b23d4e598143ab97ef7e
erywelton <ery_wel_@hotmail.com>
2023-08-26 11:21:05 -0300

    adicionando funçoes de criação e listagen de itens

--- app/controllers/item.controllers.js
+++ app/controllers/item.controllers.js
@@ -1,13 +1,51 @@
 const db = require("../models");
-const Items = db.item;
+const Item = db.item;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
+    if (!req.body.name) {
+        res.status(400).send({
+            messege: "O conteúdo não pode ser vazio!"
+        });
+        return;
+}
 
+const item = {
+
+        name: req.body.name,
+        description: req.body.description,
+        quantify: req.body.quantify,
+        is_flammable: req.body.is_flammable ? 
+        req.body.is_flammable : false
+}
+
+    Item.create(item)
+        .then(data => {
+            res.send(data);
+        })
+        .cath(err => {
+            res.status(500).send({
+                Message:
+                err.Message || "Ocorreu um erro ao criar o item."
+        })
+    })
 };
 
 exports.findAll = (req, res) => {
+        const name = req.body.name;
+        var condition = name ? {
+            name: { [Op.like]: `%${name}% `}} : null;
 
+    items.findAll({ where: condition})
+        .then(data => {
+            res.send(data);
+        })
+        .cath(err => {
+            res.status(500).send({
+                Message:
+                err.Message || "Ocorreu um erro ao listar os itens."
+    })
+})
 };
 
 exports.findOne = (req, res) => {
