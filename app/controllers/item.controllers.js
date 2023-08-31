68ad4597d5b1cc94a91f0b7cfb6f9ae61dc59c6a
erywelton <ery_wel_@hotmail.com>
2023-08-31 11:11:28 -0300

    Gerando fluxo de rotas e pesquisas de itens inflamáveis.

--- app/controllers/item.controllers.js
+++ app/controllers/item.controllers.js
@@ -1,6 +1,6 @@
-const db = require("../models");
-const Item = db.item;
-const Op = db.Sequelize.Op;
+    const db = require("../models");
+    const Item = db.item;
+    const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
     if (!req.body.name) {
@@ -49,23 +49,102 @@ exports.findAll = (req, res) => {
 };
 
 exports.findOne = (req, res) => {
+        const id = req.params.id;
 
-};
+    item.findByPK(id)
+    .then(data =>{
+        if (data){
+        res.send(data);
+        }else{
+        res.status(404).send({
+            message: `Não foi possível encontrar um item com o id=${id}.`
+        });
+        }
+    })
+    .catch(err =>{
+        res.status(500).send({
+        message: "Ocorreu um erro ao tentar encontrar um item com o id =" +id
+        });
+    })
+    };
+
 
 exports.update = (req, res) => {
+        const id = req.params.id;
 
-};
+        Item.update(req.body, {
+        where: {id: id}
+        })
+        .then(num => {
+        if(num == 1){
+            res.send({
+            message: "O item foi atualizado."
+            });
+        }else{
+            res.send({
+            message: `Não foi possivel atualizar o item com o id=${id}.`  
+            })
+        }
+        })
+        .catch(err =>{
+        res.status(500).send({
+            message: "Ocorreu um erro ao tentar atualizar um item com o id = " + id
+        });
+        })
+        
+    };
 
 exports.delete = (req, res) => {
+        const id = req.params.id;
 
-};
+    Item.destroy({
+        where: {id: id}
+    })
+    .then(num => {
+        if(num == 1){
+        res.send({
+            message: "O item foi apagado com sucesso!"
+        })
+        } else {
+        res.send({
+            message: `Nâo foi possivel apagar o item com o id= ${id}`
+        })
+        }
+    })
+    .catch(err =>{
+        res.status(500).send({
+        message: "Ocorreu um erro ao tentar apagar o item com o id=" +id
+        })
+    })
+    };
 
 exports.deleteAll = (req, res) => {
-
+    Item.destroy({
+        where: {},
+        truncate: false
+    })
+    .then(nums => {
+        res.send({ message: `${nums} Itens foram apagados com sucesso.`});
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+            err.message || "Algum erro ocorreu ao tentar apagar todos os itens."
+        });
+    });
 };
 
 exports.findAllFlammabes = (req, res) => {
-
+item.findAll({ where: { isFlammable: true } })
+.then(data => {
+    res.send(data);
+})
+.catch(err => {
+    res.status(500).send({
+        message:
+        err.message || "Algum erro ocorreu ao tentar pesquisar todos os itens inflamáveis."
+    })
+});
 };
 
 
