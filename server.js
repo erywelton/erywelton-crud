3616041a5b3b247a04bb86e29c8ecc44baf7b8db
erywelton <ery_wel_@hotmail.com>
2023-09-05 11:10:04 -0300

    mas uma atualização

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-const express = require ("express");
-const cors = require ("cors");
+    const express = require ("express");
+    const cors = require ("cors");
 
-const app = express();
+    const app = express();
 
-var corsOptions = {
-    origin: "http://localhost:8081"
-}
+    var corsOptions = {
+        origin: "http://localhost:8081"
+    }
 
-app.use(cors(corsOptions));
+    app.use(cors(corsOptions));
 
-app.use(express.json());
+    app.use(express.json());
 
-app.use(express.urlencoded({extended: true}));
+    app.use(express.urlencoded({extended: true}));
 
-const db = require("./app/models");
-db.sequelize.sync().then(() => {
-console.log("Banco conectado!");
-})
-.catch((err) => {
-    console.log("Falha ao acessar banco de dados: " + err.message);
-})
+    const db = require("./app/models");
+    db.sequelize.sync().then(() => {
+    console.log("Banco conectado!");
+    })
+    .catch((err) => {
+        console.log("Falha ao acessar banco de dados: " + err.message);
+    })
 
-app.get("/", (req,res) => {
-   res.json({message: 'hello world!'})
-});
+    require("./app/routes/item.routes")(app);
 
-const PORT = process.env.PORT ||  8090;
+    app.get("/", (req,res) => {
+    res.json({message: 'hello world!'})
+    });
 
-app.listen(PORT, () => {
-    console.log(` Server funcionando na porta ${PORT}.`);    
-});
+    const PORT = process.env.PORT ||  8090;
+
+    app.listen(PORT, () => {
+        console.log(` Server funcionando na porta ${PORT}.`);    
+    });
